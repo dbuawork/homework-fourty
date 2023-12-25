40824eab1805ca5e5582820caf1223ae44b13a88
dbuawork <dbuawork@gmail.com>
2023-12-25 22:46:01 +0200

    fourty-task-edit

--- main.py
+++ main.py
@@ -23,19 +23,27 @@
 #         num += 1
 # print(f"Число {numdigits} зустрічається {num} разів")
 
+#
+# string = input("Введіть з клавіатури рядок: ")
+# search_word = input("Введіть слово яке потрібно знайти: ")
+# replace_word = input("Введіть слово яким потрібно замінить: ")
+#
+# words = string.split()
+# modified_words = []
+#
+# for word in words:
+#    if word == search_word:
+#        modified_words.append(replace_word)
+#    else:
+#        modified_words.append(word)
+# modified_string = " ".join(modified_words)
+# print(modified_string)
 
-string = input("Введіть з клавіатури рядок: ")
-search_word = input("Введіть слово яке потрібно знайти: ")
-replace_word = input("Введіть слово яким потрібно замінить: ")
-
-words = string.split()
-modified_words = []
-
-for word in words:
-   if word == search_word:
-       modified_words.append(replace_word)
-   else:
-       modified_words.append(word)
-modified_string = " ".join(modified_words)
-print(modified_string)
+river = "Dmytro"
+print(river[:])
+print(river[2:3])
+print(river[4:5])
+print(river[:5])
+print(river[:4])
+print(river[])
 
