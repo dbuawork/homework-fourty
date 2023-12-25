9857efa2e407be490eb67276ce822ca6ee7f22ca
dbuawork <dbuawork@gmail.com>
2023-12-25 22:03:55 +0200

    finnaly stage

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ digit_count = 0
 for char in string:
     if char.isalpha():
         letter_count += 1
+    elif char.isdigit():
+        digit_count += 1
+print("Кількість літер у рядку: ", letter_count)
